af508302913ba719c550c26a214260c08a4f5f94
João Arthur <joao.obarbosa@dataprev.gov.br>
2025-10-30 11:16:32 -0300

    creating form and default input.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import "./styles/theme.css";
 import "./styles/global.css";
 
 import { Container } from "./components/Container";
-import { Heading } from "./components/Heading";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+import { Countdown } from "./components/Countdown";
+import { DefaultInput } from "./components/DefaultInput.tsx";
 
 export function App() {
   return (
@@ -13,13 +15,32 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
+
       <Container>
-        <Heading>FORM</Heading>
+        <Countdown />
       </Container>
+
       <Container>
-        <Heading>FOOTER</Heading>
+        <form className="form">
+          <div className="formRow">
+            <DefaultInput labelText="Qualquer coisa" id="meuInput" type="text" title="TITULO" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
---
+++ src/components/Countdown.tsx
@@ -0,0 +1,5 @@
+import styles from "./Countdown.module.css";
+
+export function Countdown() {
+  return <div className={styles.container}>00:00</div>;
+}
