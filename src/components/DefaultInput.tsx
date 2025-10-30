4519eddaf774f254750f7dff43b4afa6ec8bcbff
João Arthur <joao.obarbosa@dataprev.gov.br>
2025-10-30 11:16:32 -0300

    creating form and default input.

---
+++ src/components/DefaultInput.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+type DefaultInputProps = {
+    id: string;
+    labelText?: string;
+} & React.ComponentProps<'input'>;
+
+export function DefaultInput({ id, type, labelText, ...rest }: DefaultInputProps) {
+    return (
+        <>
+            {labelText && <label htmlFor={id}>{labelText}</label>}
+            <input id={id} type={type} {...rest} />
+        </>
+    );
+}
